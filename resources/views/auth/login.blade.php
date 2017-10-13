e4e045e282ddfdba28f4be9c52bb5cbc80b3c0a6
Jimmy Zhuo <zhuomingliang01@qq.com>
2017-10-13 14:29:02 +0800

    fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">登录</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                    <form class="form-horizontal" method="POST" action="/login">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
